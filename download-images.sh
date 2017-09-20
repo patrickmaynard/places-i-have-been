d7bcb61f2b18fbf8c5d428b9973735dc1fbad653
patrickmaynard <patrickmaynard@users.noreply.github.com>
2017-09-20 20:18:13 +0200

    Adds main download script

---
+++ download-images.sh
@@ -0,0 +1,58 @@
+cd ~/Pictures/
+mkdir screensaver-images
+cd screensaver-images/
+
+curl -o image01.jpg https://upload.wikimedia.org/wikipedia/commons/b/b5/Berlin_-_Flughafen_Tempelhof_-_2016.jpg
+
+curl -o image02.jpg https://upload.wikimedia.org/wikipedia/commons/7/74/Berlin_Brandenburger_Tor_Nacht.jpg
+
+curl -o image03.jpg https://upload.wikimedia.org/wikipedia/commons/6/64/View_from_Park_inn_Berlin_November_2013.jpg
+
+curl -o image04.jpg https://upload.wikimedia.org/wikipedia/commons/c/c4/Berliner_Dom_-_Berlin_Cathedral_%282012%29.JPG
+
+curl -o image05.jpg https://upload.wikimedia.org/wikipedia/commons/8/86/Berlin_Panorama_mit_Fernsehturm.jpg
+
+curl -o image06.jpg https://upload.wikimedia.org/wikipedia/commons/9/9b/Eisenbahnbr%C3%BCcke_1_G%C3%B6rlitzer_Bahn_Landwehrkanal_Berlin.JPG
+
+curl -o image07.jpg https://upload.wikimedia.org/wikipedia/commons/b/b7/Mahmal_KZ_Columbia%2C_Berlin%2C_Columbiadamm.jpg
+
+curl -o image08.jpg https://upload.wikimedia.org/wikipedia/commons/b/ba/C-47s_at_Tempelhof_Airport_Berlin_1948.jpg
+
+curl -o image09.jpg https://upload.wikimedia.org/wikipedia/commons/e/e8/Tempelhof_EDDI_2009-05-12_08.JPG
+
+curl -o image10.jpg https://upload.wikimedia.org/wikipedia/commons/a/a7/Dauair_THF.JPG
+
+curl -o image11.jpg https://upload.wikimedia.org/wikipedia/commons/a/a1/Anzeigetafel_Flughafen_Tempelhof_Berlin_20080424.jpg
+
+curl -o image12.jpg https://upload.wikimedia.org/wikipedia/commons/8/8a/Abfertigungshalle.Flughafen.Tempelhof.Berlin.2017.jpg
+
+curl -o image13.jpg https://upload.wikimedia.org/wikipedia/commons/a/a5/Tempelhofer_Flugfeld_Mai_2010.jpg
+
+curl -o image14.jpg https://upload.wikimedia.org/wikipedia/commons/9/9b/View_Berlin_TV_Tower_Jan2015_img4.jpg
+
+curl -o image15.jpg https://upload.wikimedia.org/wikipedia/commons/2/22/Galeries_Lafayette_Berlin_Glastrichter_2.jpg
+
+curl -o image16.jpg https://pixnio.com/free-images/2017/03/20/2017-03-20-12-10-03.jpg
+
+curl -o image17.jpg https://static.pexels.com/photos/163367/olympic-stadium-berlin-1936-symmetrical-163367.jpeg
+
+curl -o image18.jpg https://static.pexels.com/photos/416937/pexels-photo-416937.jpeg
+
+curl -o image19.jpg https://upload.wikimedia.org/wikipedia/commons/a/ae/Z%C3%A1mek_Vset%C3%ADn.JPG
+
+curl -o image20.jpg http://www.publicdomainpictures.net/pictures/130000/velka/becva-jaro.jpg
+
+curl -o image21.jpg https://www.goodfreephotos.com/albums/united-states/maryland/baltimore/night-time-at-the-dock-baltimore-maryland.jpg
+
+curl -o image22.jpg https://upload.wikimedia.org/wikipedia/commons/b/b8/Patterson_Park_Observatory_Bmore.JPG
+
+curl -o image23.jpg https://upload.wikimedia.org/wikipedia/commons/d/d8/Baltimore_%26_Ohio_Railroad%2C_Howard_Street_Tunnel%2C_1300_Mount_Royal_Avenue_%28Baltimore%2C_Maryland%29.jpg
+
+curl -o image24.jpg http://www.publicdomainpictures.net/pictures/30000/velka/baltimore-inner-harbor.jpg
+
+curl -o image25.jpg https://static.pexels.com/photos/210570/pexels-photo-210570.jpeg
+
+curl -o image26.jpg https://upload.wikimedia.org/wikipedia/commons/3/3f/Mi-cap-Dome-Ceiling.jpg
+
+curl -o image27.jpg https://upload.wikimedia.org/wikipedia/commons/a/a2/MSU_Beaumont_Tower_1.jpg
+
